e471ffd31597f1b4cfa4b04f7ea84ab27625d5e2
Marcelo Matos <aluno23028@ipt.pt>
2022-04-05 16:07:28 +0100

    novo branch

--- Vets/Vets/Models/Donos.cs
+++ Vets/Vets/Models/Donos.cs
@@ -20,19 +20,30 @@ namespace Vets.Models
         /// Nome do Dono do animal
         /// </summary>
         [Required(ErrorMessage ="O {0} é de preenchimento obrigatório")]
+        [StringLength(30, ErrorMessage = "O {0} não pode ter mais de {1} caracteres.")]
+        [RegularExpression("[A-ZÂÓÍa-záàç '-]+",ErrorMessage = "No {0} só são aceites letras")]
         public string Nome { get; set; }
         /// <summary>
         /// NIF do Dono do animal
         /// </summary>
         [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+        [StringLength(9, MinimumLength = 9 ,ErrorMessage ="O {0} deve ter {1} caracteres")]
+        [RegularExpression("[123578]+[0-9]{8}", ErrorMessage = "O {0} deve começar por 1, 2, 3, 5, 7 ou 8 e só ter algarismos")]
         public string NIF { get; set; }
         /// <summary>
         /// sexo do dono
         /// Ff - feminino; Mm - masculino
         /// </summary>
+        [StringLength(1, ErrorMessage ="O {0} só aceita um carácter")]
+        [RegularExpression("[FfMm]", ErrorMessage = "Apenas letras F ou M")]
         public string Sexo { get; set; }
         /// <summary>
         /// Lista de animais de quem o Dono é dono
+        /// </summary>
+        [EmailAddress(ErrorMessage ="Introduza um email correto, por favor.")]
+        public string Email { get; set; }
+        /// <summary>
+        /// Email
         /// </summary>
         public ICollection<Animais> ListaAnimais { get; set; }
     }
